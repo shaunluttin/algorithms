0336faeb01cc8b073b8312cbe8b289657bbc7f0e
Shaun Luttin <shaunluttin@gmail.com>
2018-01-25 08:23:03 -0800

    Stub tests for Algorithm A

--- articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp.Tests/Tests.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp.Tests/Tests.fs
@@ -4,5 +4,18 @@ open System
 open Xunit
 
 [<Fact>]
-let ``My test`` () =
+let ``permute1`` () = 
     Assert.True(true)
+
+[<Fact>]
+let ``mapinsert`` () = 
+    Assert.True(true)
+
+[<Fact>]
+let ``insert`` () = 
+    Assert.True(true)
+
+[<Fact>]
+let ``put`` () = 
+    Assert.True(true)
+
