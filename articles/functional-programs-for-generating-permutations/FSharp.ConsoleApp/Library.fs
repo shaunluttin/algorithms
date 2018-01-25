ce66188fe4173e459da7fd89758a682958464dab
Shaun Luttin <shaunluttin@gmail.com>
2018-01-25 08:00:20 -0800

    Add functional permutations console app

---
+++ articles/functional-programs-for-generating-permutations/FSharp.ConsoleApp/Library.fs
@@ -0,0 +1,7 @@
+namespace FSharp.ConsoleApp
+
+module Permutations =
+    let permute1 x = raise (System.NotImplementedException())
+    let mapinsert a ps = raise (System.NotImplementedException())
+    let insert a p q ps = raise (System.NotImplementedException())
+    let put a p q = raise (System.NotImplementedException())
