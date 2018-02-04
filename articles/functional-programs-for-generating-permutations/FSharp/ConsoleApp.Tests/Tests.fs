753f89ebf1ee2c1179dae723e0b0bb2d170fa2bc
Shaun Luttin <shaunluttin@gmail.com>
2018-02-03 23:01:23 -0800

    Pass put

--- articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp.Tests/Tests.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp.Tests/Tests.fs
@@ -7,19 +7,19 @@ open System
 open Xunit
 open FSharp.ConsoleApp
 
-[<Theory>]
+[<Theory(Skip = "TODO")>]
 [<MemberData("permute1TestValues")>]
 let ``permute1`` (x, expected) = 
     // Arrange // Act // Assert
     Assert.True(false)
 
-[<Theory>]
+[<Theory(Skip = "TODO")>]
 [<MemberData("mapinsertTestValues")>]
 let ``mapinsert`` (a, ps, expected) = 
     // Arrange // Act // Assert
     Assert.True(false)
 
-[<Theory>]
+[<Theory(Skip = "TODO")>]
 [<MemberData("insertTestValues")>]
 let ``insert`` (a, p, q, ps, expected) = 
     // Arrange // Act // Assert
@@ -28,5 +28,17 @@ let ``insert`` (a, p, q, ps, expected) =
 [<Theory>]
 [<MemberData("putTestValues")>]
 let ``put`` (a, p, q, expected) = 
-    // Arrange // Act // Assert
-    Assert.True(false)
+    // Arrange // Act                       
+    let actual = Permutations.put a p q     
+                                            
+    // Assert                               
+    Assert.Equal<int>(expected, actual);    
+                                            
+
+let putTestValues : obj array seq =                           
+    seq {                                                     
+        yield [|0; [1;2;3]; List.empty<int>; [1;2;3;0]|]      
+        yield [|0; [1;2;3]; [3]; [1;2;0;3]|]                  
+        yield [|0; [1;2;3]; [2;3]; [1;0;2;3]|]                
+        yield [|0; [1;2;3]; [1;2;3]; [0;1;2;3]|]              
+    }                                                         
