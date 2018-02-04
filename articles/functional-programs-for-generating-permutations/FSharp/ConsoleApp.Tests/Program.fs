0857082b44d70231b312fa8e1360ef36b4a73243
Shaun Luttin <shaunluttin@gmail.com>
2018-02-04 00:43:06 -0800

    Add usage example

--- articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp.Tests/Program.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp.Tests/Program.fs
@@ -10,4 +10,6 @@ let main argv =
     printfn "[0;1;2].Head %A" [0;1;2].Head
     printfn "[0;1;2].Tail %A" [0;1;2].Tail
 
+    Permutations.permute1 [1;2;3] |> List.iter (printf "%A ")
+
     0 // return an integer exit code
