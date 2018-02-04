6daf9d59049e44a93a671f59b0d7e0323c412960
Shaun Luttin <shaunluttin@gmail.com>
2018-02-03 23:24:13 -0800

    Pass mapinsert

--- articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp.Tests/Tests.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp.Tests/Tests.fs
@@ -13,11 +13,14 @@ let ``permute1`` (x, expected) =
     // Arrange // Act // Assert
     Assert.True(false)
 
-[<Theory(Skip = "TODO")>]
+[<Theory>]
 [<MemberData("mapinsertTestValues")>]
 let ``mapinsert`` (a, ps, expected) = 
-    // Arrange // Act // Assert
-    Assert.True(false)
+    // Arrange // Act                             
+    let actual = Permutations.mapinsert a ps      
+                                                
+    // Assert                                     
+    Assert.Equal<List<int>>(expected, actual);          
 
 [<Theory>]
 [<MemberData("insertTestValues")>]
@@ -40,14 +43,20 @@ let ``put`` (a, p, q, expected) =
 
 let putTestValues : obj array seq =                           
     seq {                                                     
-        yield [|0; [1;2;3]; List.empty<int>; [1;2;3;0]|]      
-        yield [|0; [1;2;3]; [3]; [1;2;0;3]|]                  
-        yield [|0; [1;2;3]; [2;3]; [1;0;2;3]|]                
-        yield [|0; [1;2;3]; [1;2;3]; [0;1;2;3]|]              
+        yield [| 0; [1;2;3]; List.empty<int>; [1;2;3;0] |]      
+        yield [| 0; [1;2;3]; [3]; [1;2;0;3] |]                  
+        yield [| 0; [1;2;3]; [2;3]; [1;0;2;3] |]                
+        yield [| 0; [1;2;3]; [1;2;3]; [0;1;2;3] |]              
     }                                                         
 
 let insertTestValues : obj array seq =                                                            
     seq {                                                                                         
-        yield [|0; [1;2]; [2]; List.empty<List<int>>; [[1;0;2]; [1;2;0]]|]
+        yield [| 0; [1;2]; [2]; List.empty<List<int>>; [[1;0;2]; [1;2;0]] |]
+    }                                                                                             
+                                                                                                  
+let mapinsertTestValues : obj array seq =                                                            
+    seq {                                                                                         
+        yield [| 0; List.empty<List<int>>; List.empty<List<int>> |]
+        yield [| 1; [[0]]; [[1;0]; [0;1]] |]
     }                                                                                             
                                                                                                   
