aab2a172be8308c33aa4d4ead7b7ee2745d1ed69
Shaun Luttin <shaunluttin@gmail.com>
2018-02-22 21:20:37 -0800

    Make module recursive

--- articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp/Permute2.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/ConsoleApp/Permute2.fs
@@ -1,8 +1,8 @@
 namespace FSharp.ConsoleApp
 
-module Permute2 =
+module rec Permute2 =
 
-    let removeFirst (x: List<'t>) (y: 't)
+    let removeFirst (x: List<'t>) (y: 't) =
         raise (System.NotImplementedException(""))
 
     let rec mapcons (a: 't) (ps: List<List<'t>>) (qs: List<List<'t>>): List<List<'t>> =
@@ -12,9 +12,6 @@ module Permute2 =
         raise (System.NotImplementedException(""))
         let mp = mapperm x y.Tail
         let rm = removeFirst x y.Head
-
-        // How do we call permute2 from mapperm 
-        // and call mapperm from permute2?
         let pm = permute2 rm
         mapcons y.Head pm mp
 
