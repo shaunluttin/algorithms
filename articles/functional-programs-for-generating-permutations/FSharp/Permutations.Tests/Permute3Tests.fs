95519e5c3a07c429c033e93a03931dcc00138efb
Shaun Luttin <shaunluttin@gmail.com>
2018-03-14 16:36:48 -0700

    Introduce ReverseLexographic test data

--- articles/functional-programs-for-generating-permutations/FSharp/Permutations.Tests/Permute3Tests.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/Permutations.Tests/Permute3Tests.fs
@@ -4,21 +4,10 @@ open Xunit
 open Permutations.Permute3
 
 [<Theory>]
-[<MemberData("permuteTestData")>]
+[<ClassData(typeof<TestData.PermuteReverseLexographic>)>]
 let ``permute`` (x, expected: 't list list) =
     // Arrange // Act
     let actual = permute x
 
     // Assert
     Assert.Equal<'t list list>(expected, actual)
-
-let permuteTestData : obj array seq = 
-    seq {
-        yield [| List.empty<int>; [ List.empty<int> ] |]
-        yield [| [0]; [ [0] ] |]
-        yield [| [0;1]; [ [0;1]; [1;0] ] |]
-        yield [| 
-            ["A";"B";"C"];
-            [["A";"B";"C"];["B";"A";"C"];["A";"C";"B"];["C";"A";"B"];["B";"C";"A"];["C";"B";"A"]] 
-        |]
-    }
--- articles/functional-programs-for-generating-permutations/FSharp/Permutations.Tests/TestData.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/Permutations.Tests/TestData.fs
@@ -1,18 +1,35 @@
 module TestData
 
-type PermuteLexographic () = 
-    let values : obj array seq =                                                            
-        seq {                                                                                         
-            yield [| ["A";"B"]; [["A";"B"]; ["B";"A"]] |]
+type ClassDataBase (generator: obj array seq) = 
+    interface obj array seq with 
+        member this.GetEnumerator () = generator.GetEnumerator()
+        member this.GetEnumerator () = 
+            generator.GetEnumerator() :> System.Collections.IEnumerator
 
+
+type PermuteLexographic () = 
+    inherit ClassDataBase(seq 
+        {                                                                                         
+            yield [| 
+                ["A";"B"]; 
+                [["A";"B"]; ["B";"A"]] 
+            |]
             yield [| List.empty<int>; [ List.empty<int> ] |]
             yield [| [0]; [[0]] |]
             yield [| [0;1]; [[0;1];[1;0]] |]
-
             yield [| [0;1;2]; [[0;1;2]; [0;2;1]; [1;0;2]; [1;2;0]; [2;0;1]; [2;1;0]] |]
-        }                                                                                             
+        })
+                                                                                                 
 
-    interface seq<obj[]> with 
-        member this.GetEnumerator () = values.GetEnumerator()
-        member this.GetEnumerator () = 
-            values.GetEnumerator() :> System.Collections.IEnumerator
+
+type PermuteReverseLexographic () = 
+    inherit ClassDataBase(seq 
+        {
+            yield [| 
+                ["A";"B";"C"];
+                [["A";"B";"C"];["B";"A";"C"];["A";"C";"B"];["C";"A";"B"];["B";"C";"A"];["C";"B";"A"]] 
+            |]
+            yield [| List.empty<int>; [ List.empty<int> ] |]
+            yield [| [0]; [ [0] ] |]
+            yield [| [0;1]; [ [0;1]; [1;0] ] |]
+        })
