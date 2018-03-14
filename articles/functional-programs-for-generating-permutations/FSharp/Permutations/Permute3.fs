50615af1f270b5668250a941129fd9304ebb929f
Shaun Luttin <shaunluttin@gmail.com>
2018-03-13 17:48:39 -0700

    Pass first permute3 test

--- articles/functional-programs-for-generating-permutations/FSharp/Permutations/Permute3.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/Permutations/Permute3.fs
@@ -1,6 +1,18 @@
 module rec Permutations.Permute3
 
-let permute x =
+let mapperm (x: 't list) (y: 't list) (p: 't list) (ps: 't list list) = 
+    match y with 
+    | [] -> ps
+    | head::tail -> 
+        let genpermNext = genperm (Permute2.removeFirst x head) (head::p) ps
+        mapperm x tail p genpermNext
+
+let genperm (x: 't list) (p: 't list) (ps: 't list list) = 
+    match x.Tail with 
+    | [] -> (x.Head :: p) :: ps
+    | _ -> mapperm x x p ps
+
+let permute (x: 't list): 't list list =
     match x with 
     | [] -> [ [] ]
-    | _ -> genperm x [] []
+    | _ -> genperm x [] [[]]
