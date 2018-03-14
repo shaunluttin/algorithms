1b2e9fdb1cf6dbf4e20c96b93f669ddb2789b3ca
Shaun Luttin <shaunluttin@gmail.com>
2018-03-13 17:50:25 -0700

    Pass third permute3 test

--- articles/functional-programs-for-generating-permutations/FSharp/Permutations.Tests/Permute3Tests.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/Permutations.Tests/Permute3Tests.fs
@@ -16,7 +16,7 @@ let permuteTestData : obj array seq =
     seq {
         yield [| List.empty<int>; [ List.empty<int> ] |]
         yield [| [0]; [ [0] ] |]
-        // yield [| [0;1]; [ [0;1]; [1;0] ] |]
+        yield [| [0;1]; [ [0;1]; [1;0] ] |]
         // yield [| 
         //     ["A";"B";"C"];
         //     [["A";"B";"C"];["B";"A";"C"];["A";"C";"B"];["C";"A";"B"];["B";"C";"A"];["C";"B";"A"]] 
