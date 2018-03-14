76f41c5b872b90423bc221c41c65eefcbee9a1e3
Shaun Luttin <shaunluttin@gmail.com>
2018-03-13 17:12:12 -0700

    Short generics notation

--- articles/functional-programs-for-generating-permutations/FSharp/Permutations/Permute2.fs
+++ articles/functional-programs-for-generating-permutations/FSharp/Permutations/Permute2.fs
@@ -1,25 +1,25 @@
 module rec Permutations.Permute2
 
-    let removeFirst (list: List<'t>) (item: 't): List<'t> =
-        match list with
-        | [] -> []
-        | head::tail when head = item -> tail
-        | head::tail -> head :: (removeFirst tail item)
+let removeFirst (list: 't list) (item: 't) : 't list =
+    match list with
+    | [] -> []
+    | head::tail when head = item -> tail
+    | head::tail -> head :: (removeFirst tail item)
 
-    let rec mapcons (a: 't) (ps: List<List<'t>>) (qs: List<List<'t>>): List<List<'t>> =
-        match ps with
-        | [] -> qs
-        | head::tail -> (a :: head) :: mapcons a tail qs
+let mapcons (a: 't) (ps: 't list list) (qs: 't list list): 't list list =
+    match ps with
+    | [] -> qs
+    | head::tail -> (a :: head) :: mapcons a tail qs
 
-    let rec mapperm (x: List<'t>) (y: List<'t>): List<List<'t>> = 
-        match y with 
-        | [] -> []
-        | head::tail -> 
-            let permuteNext = permute (removeFirst x head)
-            let mappermNext = mapperm x tail
-            mapcons head permuteNext mappermNext
+let mapperm (x: 't list) (y: 't list): 't list list = 
+    match y with 
+    | [] -> []
+    | head::tail -> 
+        let permuteNext = permute (removeFirst x head)
+        let mappermNext = mapperm x tail
+        mapcons head permuteNext mappermNext
 
-    let rec permute (x: List<'t>) : List<List<'t>> = 
-        match x with 
-        | [] -> [ [] ]
-        | _ -> mapperm x x
+let permute (x: 't list) : 't list list = 
+    match x with 
+    | [] -> [ [] ]
+    | _ -> mapperm x x
