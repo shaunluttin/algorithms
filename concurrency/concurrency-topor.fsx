0752c6c23383eaeba32e052380c173b9663ae69b
Shaun Luttin <shaunluttin@gmail.com>
2018-03-22 21:28:08 -0700

    Implement insert with list comprehension

--- concurrency/concurrency-topor.fsx
+++ concurrency/concurrency-topor.fsx
@@ -1,9 +1,13 @@
-
-
+// inserts a in to xs immediately after q
 let put a q xs = 
     xs |> Seq.fold 
-        (fun acc x -> match x with 
-                        | x when x = q -> a::x::acc
-                        | _ -> x::acc) []
+        (fun acc x -> 
+            match x with 
+                | x when x = q -> acc@[x;a]
+                | _ -> acc@[x]) []
 
-["A";"B";"C"] |> put "0" "A"
+// puts a at each possible position of xs
+let insert a xs =             
+    xs |> Seq.fold (fun acc x -> (put a x xs)::acc) [a::xs] 
+ 
+insert "0" ["A";"B";"C"] 
