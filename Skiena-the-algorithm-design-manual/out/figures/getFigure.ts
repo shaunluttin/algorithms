b5c67bb229a307f115f3daffc3f0ea2538ab174f
Shaun Luttin <admin@shaunluttin.com>
2020-07-10 18:51:00 -0700

    Introduce typescript-implementations/; delete out/

--- Skiena-the-algorithm-design-manual/out/figures/getFigure.ts
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export default (figure: number): string =>
-  fs.readFileSync(path.join(__dirname, `figure-${figure}.txt`), {
-    encoding: "utf8",
-  });
